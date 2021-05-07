9caf0b7a9ece9a14b3ebe293237fa272fe146df3
kaio alves <kaio.alves19@gmail.com>
2021-05-07 18:22:46 -0300

    Atualização do conteudo do Index.html, adição do arquivo js,

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -3,10 +3,23 @@ const novaTarefa = document.querySelector('[data-form-button]')
 const criarTarefa = (evento) =>{
 
     evento.preventDefault()
-    const input=document.querySelector('[data-form-input]')
+
+    const lista= document.querySelector('[data-lista]')
+    const input= document.querySelector('[data-form-input]')
     const valor= input.value
-    console.log(valor)
-    // input.value=" "
+    
+    const tarefa= document.createElement('li')
+    tarefa.classList.add('task')
+    
+    const conteudo = `<p class="content">${valor}</p>`
+
+    tarefa.innerHTML=conteudo
+    lista.appendChild(tarefa)
+
+    
+
+    input.value=" "
+
 }
 
 novaTarefa.addEventListener('click', criarTarefa )
